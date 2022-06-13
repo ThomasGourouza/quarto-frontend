1de8601f7d6adf8f479ef4d9ca9809a5b5bf224a
vagrant <vagrant@vagrant.vm>
2022-06-13 12:11:46 +0000

    add click conditions

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Game } from 'src/app/models/game';
 import { Piece } from 'src/app/models/piece.model';
 import { Position } from 'src/app/models/position';
@@ -10,48 +11,66 @@ import { GameService } from 'src/app/services/game.service';
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent implements OnInit {
+export class GameComponent implements OnInit, OnDestroy {
 
   game: Game = new Game();
+
+  private gameSubscription = new Subscription();
 
   constructor(private gameService: GameService) { }
 
   ngOnInit(): void {
+    this.gameSubscription = this.gameService.game$.subscribe((game) => this.game = game);
+  }
+
+  ngOnDestroy(): void {
+    this.gameSubscription.unsubscribe();
   }
 
   onCreateGame(): void {
     this.gameService.createGame({
-      name: "partie test3",
+      name: "partie test1",
       description: "test",
       player1: "premier",
       player2: "deuxième"
-    }).subscribe((game) => this.game = game);
+    });
   }
 
-  onSquareClick(square: Square | null): void {
-    if (!!square) {
-      this.gameService.play(this.game.id, { row: square.row, column: square.column })
-        .subscribe((game) => this.game = game);
+  onSquareClick(square: Square | null, type: 'board' | 'set'): void {
+    if (!!square && (this.playConditionForBoard(square, type) || this.playConditionForSet(square, type))) {
+      this.gameService.play(this.game.id, { row: square.row, column: square.column });
     }
   }
 
+  private playConditionForBoard(square: Square, type: 'board' | 'set'): boolean {
+    return type === 'board' && !!this.getLast(this.game.positions).currentPiece && !square.piece;
+  }
+
+  private playConditionForSet(square: Square, type: 'board' | 'set'): boolean {
+    return type === 'set' && !this.getLast(this.game.positions).currentPiece && !!square.piece;
+  }
+
   getCurrentRank(positions: Position[]): number {
-    return positions[positions.length - 1].rank;
+    return this.getLast(positions).rank;
   }
 
   getCurrentPlayer(positions: Position[]): string {
-    return positions[positions.length - 1].currentPlayer;
+    return this.getLast(positions).currentPlayer;
   }
 
   getCurrentPiece(positions: Position[]): Piece | null {
-    return positions[positions.length - 1].currentPiece;
+    return this.getLast(positions).currentPiece;
   }
 
   getBoard(positions: Position[]): Square[] {
-    return positions[positions.length - 1].board;
+    return this.getLast(positions).board;
   }
 
   getSet(positions: Position[]): Square[] {
-    return positions[positions.length - 1].set;
+    return this.getLast(positions).set;
+  }
+
+  private getLast(positions: Position[]): Position {
+    return positions[positions.length - 1];
   }
 }
--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Game } from '../models/game';
 import { PostGame } from '../models/post-game';
 
@@ -11,42 +11,65 @@ const GAME_URL = 'http://localhost:8080/games/';
 })
 export class GameService {
 
+  game$: BehaviorSubject<Game> = new BehaviorSubject(new Game());
+  games$: BehaviorSubject<Game[]> = new BehaviorSubject([new Game()]);
+
   constructor(
     private http: HttpClient
   ) { }
 
-  public createGame(game: PostGame): Observable<Game> {
-    return this.http.post<Game>(GAME_URL, game);
+  public createGame(postGame: PostGame): void  {
+    this.subscribeGame(this.http.post<Game>(GAME_URL, postGame));
   }
 
-  public getGames(): Observable<Array<Game>> {
-    return this.http.get<Array<Game>>(GAME_URL);
+  public getGames(): void {
+    this.subscribeGames(this.http.get<Array<Game>>(GAME_URL));
   }
 
-  public getGameById(id: string): Observable<Game> {
-    return this.http.get<Game>(`${GAME_URL}${id}`);
+  public getGameById(id: string): void {
+    this.subscribeGame(this.http.get<Game>(`${GAME_URL}${id}`));
   }
 
-  public getGameByName(param: { name: string }): Observable<Array<Game>> {
-    return this.http.get<Array<Game>>(`${GAME_URL}search`, {
+  public getGameByName(param: { name: string }): void {
+    this.subscribeGames(this.http.get<Array<Game>>(`${GAME_URL}search`, {
       observe: 'body',
       params: this.asHttpParam(param)
+    }));
+  }
+
+  public updateGame(id: string, game: PostGame): void {
+    this.subscribeGame(this.http.put<Game>(`${GAME_URL}${id}`, game));
+  }
+
+  public deleteGameById(id: string): void {
+    this.http.delete<void>(`${GAME_URL}${id}`).subscribe(() => {
+      console.log('deleted');
+    }).unsubscribe();
+  }
+
+  public play(id: string, move: { row: number, column: number }): void {
+    this.subscribeGame(this.http.patch<Game>(`${GAME_URL}${id}/play`, move));
+  }
+
+  private subscribeGame(observable: Observable<Game>): void {
+    observable.subscribe((game) => {
+      console.log(game);
+      if (!!game) {
+        this.game$.next(game);
+      }
     });
   }
 
-  public updateGame(id: string, game: PostGame): Observable<Game> {
-    return this.http.put<Game>(`${GAME_URL}${id}`, game);
+  private subscribeGames(observable: Observable<Game[]>): void {
+    observable.subscribe((games) => {
+      console.log(games);
+      if (!!games) {
+        this.games$.next(games);
+      }
+    });
   }
 
-  public deleteGameById(id: string): Observable<void> {
-    return this.http.delete<void>(`${GAME_URL}${id}`);
-  }
-
-  public play(id: string, move: { row: number, column: number }): Observable<Game> {
-    return this.http.patch<Game>(`${GAME_URL}${id}/play`, move);
-  }
-
-  public asHttpParam(object: any): HttpParams {
+  private asHttpParam(object: any): HttpParams {
     let httpParams = new HttpParams();
     for(const param in object) {
       httpParams = httpParams.set(param, object[param]);
